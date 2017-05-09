72b9daaf97b2d2f04f02ffe8ff41174b83e4345c
Reid Henderson <reid.henderson@gmail.com>
2017-05-08 23:03:53 -0400

    Added jaws DB connection details

--- app/data/connection.js
+++ app/data/connection.js
@@ -1,11 +1,18 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  user: "root",
-  password: "",
-  database: "friends_db"
-});
+var connection;
+
+if (process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+  } else {
+    connection = mysql.createConnection({
+      host: "localhost",
+      user: "root",
+      password: "",
+      database: "friends_db"
+    });
+}
+
 
 connection.connect(function(err) {
   if (err) console.error("error connecting: " + err.stack);
